dfce4323e03f2b4548a68ff3e634afeca5fe484d
IanWill2k16 <ianwill2014@gmail.com>
2025-12-16 13:44:57 -0600

    add oidc variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,3 +12,13 @@ variable "name_prefix" {
   type        = string
   default     = "identity-automation"
 }
+
+variable "oidc_client_id" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
+variable "subscription_id" {
+  type = string
+}
