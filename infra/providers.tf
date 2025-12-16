5b3f0ef87bd236351132efea30a6c00771795772
IanWill2k16 <ianwill2014@gmail.com>
2025-12-16 13:43:49 -0600

    add oidc to provider block

--- infra/providers.tf
+++ infra/providers.tf
@@ -11,4 +11,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.oidc_client_id
+
+  use_oidc = true
 }
